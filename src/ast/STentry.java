429cbb29293ac5dc8064e1c7e62a86d29f1c5d90
Andrea269 <a.pascali2@studenti.uniba.it>
2018-06-15 15:54:56 +0200

    add util, lib and Node interface

--- src/ast/STentry.java
+++ src/ast/STentry.java
@@ -1,35 +1,42 @@
 package ast;
-public class STentry {
- 
-  private int nl;
-  private Node type;
-  private int offset;
-  
-  public STentry(int n, int os)
-  {nl=n;
-  offset=os;} 
-   
-  public STentry(int n, Node t, int os)
-  {nl=n;
-   type=t;
-   offset=os;}
-  
-  public void addType (Node t)
-  {type=t;}
-  
-  public Node getType ()
-  {return type;}
 
-  public int getOffset ()
-  {return offset;}
-  
-  public int getNestinglevel ()
-  {return nl;}
-  
-  public String toPrint(String s) { //
-	   return s+"STentry: nestlev " + Integer.toString(nl) +"\n"+
-			  s+"STentry: type\n" + 
-			  type.toPrint(s+"  ") + 
-		      s+"STentry: offset " + Integer.toString(offset) + "\n";
-  }
+public class STentry {
+
+    private int nl;
+    private Node type;
+    private int offset;
+
+    public STentry(int n, int os) {
+        nl = n;
+        offset = os;
+    }
+
+    public STentry(int n, Node t, int os) {
+        nl = n;
+        type = t;
+        offset = os;
+    }
+
+    public void addType(Node t) {
+        type = t;
+    }
+
+    public Node getType() {
+        return type;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public int getNestinglevel() {
+        return nl;
+    }
+
+    public String toPrint(String s) { //
+        return s + "STentry: nestlev " + Integer.toString(nl) + "\n" +
+                s + "STentry: type\n" +
+                type.toPrint(s + "  ") +
+                s + "STentry: offset " + Integer.toString(offset) + "\n";
+    }
 }  
