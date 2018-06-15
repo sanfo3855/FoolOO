ecdcf550e86f7910a01b27049b45c29e0ca7575d
Andrea269 <a.pascali2@studenti.uniba.it>
2018-06-15 16:40:09 +0200

    create node classes

---
+++ src/ast/LetNode.java
@@ -0,0 +1,34 @@
+package ast;
+
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class LetNode implements Node {
+
+    private Node exp;
+
+    public LetNode (Node e) {
+        exp=e;
+    }
+
+    public String toPrint(String s) {
+
+        return "Start\n" + exp.toPrint("  ") ;
+    }
+
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+
+        return exp.checkSemantics(env);
+    }
+
+    public Node typeCheck() {
+        return exp.typeCheck();
+    }
+
+    public String codeGeneration() {
+        return exp.codeGeneration()+"halt\n";
+    }
+
+}
