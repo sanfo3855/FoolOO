61dacee3dc512ecf3e8d89b2197fb8e6bcc9693a
Andrea269 <a.pascali2@studenti.uniba.it>
2018-06-21 17:24:21 +0200

    Implemented typeCheck in Nodes

--- src/ast/FunExpNode.java
+++ src/ast/FunExpNode.java
@@ -44,40 +44,31 @@ public class FunExpNode implements Node {
         STentry tmpEntry = null;
         HashMap<String,STentry> tmpHm=null;
         HashMap<String,STentry> hmClassExt=env.getHashMapNL(0);
+        ArrayList<String> keyType= new ArrayList<String>();
+        int keylength=0;
         while (j>=0 /*&& tmpEntry==null*/){
             tmpHm = env.getHashMapNL(j--);
             for (Map.Entry<String,STentry> chkEntry : tmpHm.entrySet()) {
                 String keysharp[] = chkEntry.getKey().split("#");
                 if(keysharp[0].equals("fun")) {
-                    String key[] = keysharp[1].split("%");
-                    int keylength=key.length;
+                    String[] key = keysharp[1].split("%");
+                    for (String k:key) {
+                        keyType.add(k);
+                    }
+
+                    keylength=key.length;
                     if ((keylength-2 == listParam.size()) && (key[0].equals(id))) {
                         tmpEntry = chkEntry.getValue();
-                        for(int i=2; i<keylength-2; i++){
-                            if(key[i].equals("int")) listParam.add(new IntTypeNode());
-                            else if (key[i].equals("bool")) listParam.add(new BoolTypeNode());
-                            else listParam.add(new IdTypeNode(key[i]));
-                        }
                     }else{
                         if(typeClassMethod!=null) {
                             if (key[0].equals(id) && (keylength - 4) == listParam.size() && key[keylength - 2].equals("class") && key[keylength - 1].equals(typeClassMethod)) {
                                 tmpEntry = chkEntry.getValue();
-                                for(int i=2; i<keylength-4; i++){
-                                    if(key[i].equals("int")) listParam.add(new IntTypeNode());
-                                    else if (key[i].equals("bool")) listParam.add(new BoolTypeNode());
-                                    else listParam.add(new IdTypeNode(key[i]));
-                                }
                             } else {
                                 for (String keyfun : hmClassExt.keySet()) {
                                     String[] splitKey = keyfun.split("@");
                                     if (splitKey.length > 1 && splitKey[0].split("%")[1].equals(typeClassMethod)) {
                                         if (key[0].equals(id) && (keylength - 4) == listParam.size() && key[keylength - 2].equals("class") && key[keylength - 1].equals(splitKey[1])) {
                                             tmpEntry = chkEntry.getValue();
-                                            for(int i=2; i<keylength-4; i++){
-                                                if(key[i].equals("int")) listParam.add(new IntTypeNode());
-                                                else if (key[i].equals("bool")) listParam.add(new BoolTypeNode());
-                                                else listParam.add(new IdTypeNode(key[i]));
-                                            }
                                         }
                                     }
                                 }
@@ -86,11 +77,6 @@ public class FunExpNode implements Node {
                         }else{
                             if (key[0].equals(id) && (keylength - 4) == listParam.size() && key[keylength - 2].equals("class")) {
                                 tmpEntry = chkEntry.getValue();
-                                for(int i=2; i<keylength-4; i++){
-                                    if(key[i].equals("int")) listParam.add(new IntTypeNode());
-                                    else if (key[i].equals("bool")) listParam.add(new BoolTypeNode());
-                                    else listParam.add(new IdTypeNode(key[i]));
-                                }
                             }
                         }
                     }
@@ -100,10 +86,37 @@ public class FunExpNode implements Node {
         if(tmpEntry==null){
             semanticErrors.add(new SemanticError("Funzio id " + id + " is not declared"));
         } else {
+            int keySub;
+            if ((keylength-2 == listParam.size()) && (keyType.get(0).equals(id))) {
+                keySub=2;
+            }else{
+                keySub=4;
+            }
+            for(int i=1; i<keylength-keySub; i++){
+                if(keyType.get(i).equals("int")){
+                    if(i==1){
+                        tmpEntry.addType(new IntTypeNode());
+                    }else{
+                        listParam.add(new IntTypeNode());
+                    }
+                }
+                else if (keyType.get(i).equals("bool")) {
+                    if(i==1){
+                        tmpEntry.addType(new BoolTypeNode());
+                    }else{
+                        listParam.add(new BoolTypeNode());
+                    }
+                }
+                else {
+                    if(i==1){
+                        tmpEntry.addType(new IdTypeNode(keyType.get(i)));
+                    }else{
+                        listParam.add(new IdTypeNode(keyType.get(i)));
+                    }
+                }
+            }
             entry=tmpEntry;
         }
-
-
         //checkSemantics per listParam
         for(Node ntc : listParam){
             semanticErrors.addAll(ntc.checkSemantics(env));
@@ -113,7 +126,6 @@ public class FunExpNode implements Node {
     }
 
     public Node typeCheck() {
-        Node returnType = entry.getType();
         for(int i=0; i<listParam.size(); i++){
             System.out.println(i);
             if(!FOOLlib.isSubtype(listParam.get(i).typeCheck(),typeParam.get(i))){
@@ -121,7 +133,7 @@ public class FunExpNode implements Node {
                 System.exit(0);
             }
         }
-        return returnType;
+        return entry.getType();
     }
 
     public String codeGeneration() {
