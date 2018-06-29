4b8129dbed5c403b71282d8890d88ffab1cd12fe
matmar95 <matteo.marchesini12@studio.unibo.it>
2018-06-29 17:23:11 +0200

    Added comments and JavaDoc

--- src/ast/IdNode.java
+++ src/ast/IdNode.java
@@ -11,46 +11,74 @@ public class IdNode implements Node {
     private STentry entry;
     private int nestinglevel;
 
+    /**
+     * Constructor for IdNode
+     * @param id --> name of Node
+     */
     public IdNode(String id) {
         this.id=id;
     }
 
+    /**
+     * Print structure of IdNode
+     * @param s parent Indentation, incremented at every toPrint
+     * @return updated string that prints Abstract Syntax Tree Structure
+     */
     public String toPrint(String s) {
         return s+"Id:" + id + " at nestlev " + nestinglevel +"\n" ;
     }
 
+    /**
+     * Check IdNode's semantic.
+     * Child node: ProgNode and listVar
+     * @param env -> Environment that holds previously parsed information
+     * @return  updated ArrayList of semantic errors
+     */
     public ArrayList<SemanticError> checkSemantics(Environment env) {
+        //creo arraylist di errori semantici
         ArrayList<SemanticError> semanticErrors = new ArrayList<SemanticError>();
-
+        //ottengo in j il livello corrente
         int j=env.getNestingLevel();
         STentry tmpEntry=null;
+        //controllo iterativamente che tale id sia stato dichiarato
         while (j>=0 && tmpEntry==null){
             tmpEntry = env.getHashMapNL(j--).get(id);
         }
+        //ritorno un errore nel caso in cui non sia stato dichiarato
         if(tmpEntry==null){
-            semanticErrors.add(new SemanticError("Class Id " + id + " is not declared"));
+            semanticErrors.add(new SemanticError("id " + id + " is not declared"));
         } else {
+            //set della entry con l'id corrente
             entry = tmpEntry;
+            //set del nesting level con il livello corrente
             nestinglevel = env.getNestingLevel();
         }
-
         return semanticErrors;
     }
 
+    /**
+     * Check the type of the IdNode and return the corresponding type
+     * @return typeNode --> It's a BoolTypeNode, IntTypeNode or IdTypeNode
+     */
     public Node typeCheck() {
+        //ottiene il tipo dell'id
         String type=((TypeNode) entry.getType()).getType();
         TypeNode typeNode;
         if (type.equals("int")){
+            //identifico typeNode come un nodo IntTypeNode
             typeNode=new IntTypeNode();
         }else{
             if (type.equals("bool")){
+                //identifico typeNode come un nodo BoolTypeNode
                 typeNode=new BoolTypeNode();
             }else{
+                //identifico typeNode come un nodo IdTypeNode, e quindi un oggetto type
                 typeNode=new IdTypeNode(type);
             }
         }
         return typeNode;
     }
+
 
     public String codeGeneration() {
         return "";
--- src/ast/VarNode.java
+++ src/ast/VarNode.java
@@ -3,7 +3,6 @@ package ast;
 import lib.FOOLlib;
 import util.Environment;
 import util.SemanticError;
-
 import java.util.ArrayList;
 
 public class VarNode implements Node {
@@ -11,41 +10,58 @@ public class VarNode implements Node {
     private Node varDec;
     private Node value;
 
+    /**
+     * Constructor for VarNode
+     * @param varDec --> Node declaration
+     * @param value --> value of Node declaration
+     */
     public VarNode (Node varDec, Node value) {
         this.varDec=varDec;
         this.value=value;
     }
 
-    public VarNode (Node varDec){
-        new VarNode(varDec, null);
-    }
-
-
+    /**
+     * Prints structure of VarNode and call toPrint method on every child node
+     * @param s parent Indentation, incremented at every toPrint
+     * @return updated string that prints Abstract Syntax Tree Structure
+     */
     public String toPrint(String s) {
-
         return s + "VarNode\n" +
                 s + this.varDec.toPrint(s + "   ") + "\n" +
                 s + this.value.toPrint(s + "  ") + "\n" ;
     }
 
+    /**
+     * Checks VarNode's semantic and call checkSemantic method on every child Node
+     * @param env -> Environment that holds previously parsed information
+     * @return updated ArrayList of semantic errors
+     */
     public ArrayList<SemanticError> checkSemantics(Environment env) {
         ArrayList<SemanticError> semanticErrors = new ArrayList<SemanticError>();
+        //chiamo check semantics nel nodo vardec
         semanticErrors.addAll(varDec.checkSemantics(env));
+        //chiamo check semantics nel nodo value
         semanticErrors.addAll(value.checkSemantics(env));
-
         return semanticErrors;
     }
 
+    /**
+     * Check the subtyping relation between value and vardec.
+     * @return null
+     */
     public Node typeCheck() {
-        //System.out.println("VarNode - "+((VarDecNode)varDec).getId()+" - "+varDec.typeCheck()+" - "+value.typeCheck());
-
         if (! (FOOLlib.isSubtype(value.typeCheck(), varDec.typeCheck())) ){
+            //genero errore nel caso in cui il sottotipo non sia verificato
             System.out.println("Incompatible value for variable "+((VarDecNode)varDec).getId());
             System.exit(0);
         }
         return null;
     }
 
+    /**
+     *
+     * @return
+     */
     public String codeGeneration() {
         return value.codeGeneration()+"halt\n";
     }
