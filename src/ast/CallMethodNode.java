7c6d2d1c6ecb35dd4c3d08d270bf86f59a25b0fc
Andrea269 <a.pascali2@studenti.uniba.it>
2018-07-04 12:45:04 +0200

    implemented codeGeneration in callMethodNode

--- src/ast/CallMethodNode.java
+++ src/ast/CallMethodNode.java
@@ -136,36 +136,22 @@ public class CallMethodNode implements Node {
             sizeVarHp=Integer.parseInt(DispatcherTable.getEntry(listExtension.get(--y)).get("numberVar"));
         }
         for(int i = sizeVarHp; i>0; i--){
-            returnString += "push " +entry.getOffset()+"\n"+
-                            "lfp\n"+
-                            "add\n"+
-                            "lw\n"+
-                            "push "+(i-1)+"\n" +
-                            "add\n"+
-                            "lw\n";
+            returnString += "push " +entry.getOffset()+"\n"+"lfp\n"+"add\n"+"lw\n"+"push "+(i-1)+"\nadd\n"+"lw\n";
         }
-        returnString += "push 0\n" +
-                        "lfp\n";
+        returnString += "push 0\nlfp\nsfpo\nlfp\n";
         for (int i = methodCall.getSizeListParam()-1; i>=0; i--){
             returnString += methodCall.getListParam().get(i).codeGeneration(); //eseguo il push dei parametri del chiamante
         }
         String retHpVar="cfpm\n";
         for(int i = sizeVarHp; i>0; i--){
-            retHpVar += "push "+(i+1)+"\n" +
-                        "lfp\n" +
-                        "add\n" +
-                        "lw\n" +
-                        "push "+((-entry.getOffset()+sizeVarHp))+"\n" +
-                        "lfp\n" +
-                        "add\n" +
-                        "lw\n" +
-                        "push "+(i-1)+"\n" +
-                        "add\n" +
-                        "sw\n";//todo
+            retHpVar +="push "+(i+1)+"\nlfp\nadd\nlw\npush "+((entry.getOffset()))+"\nlfpo\nadd\nlw\npush "+(i-1)+"\nadd\nsw\n";
         }
-        return returnString+
-                "push "+bLabel+
-                "js\n"+
-                retHpVar;
+        retHpVar+="push 1\nlfp\nadd\nlw\nlfp\npush "+(sizeVarHp+1)+"\nadd\nsw\n";
+
+        for(int i = sizeVarHp; i>0; i--){
+            retHpVar +="pop\n";
+        }
+        retHpVar+="cfpo\n";
+        return returnString+"push "+bLabel+"js\n"+retHpVar;
     }
 }
